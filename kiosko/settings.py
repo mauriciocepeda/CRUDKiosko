cd10be8ab0c30d9eece7c204f4f573d43a5bf9b2
Mauricio Cepeda <mauricepeda@hotmail.com>
2023-01-16 18:00:25 -0300

    configuracion para vercel

--- kiosko/settings.py
+++ kiosko/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6mg=ch15*jftc9igts73pswj(zn*cd=!8)wb-0mey57gfz#u@f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
